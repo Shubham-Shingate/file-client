2acfa7470b1c05a387e691a428693a85e535c446
Olivia Carino <olcarino@gmail.com>
2022-03-01 15:11:55 -0500

    added comments of requirements for file-client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
     println!("Hello, world!");
+    
+    /* User Login (guest or existing user) */
+
+    /* File Operations, Print Directory Contents, Display/Indicate Hidden Directories, and File Search  */
 }
